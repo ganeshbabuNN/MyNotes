afa93dbf6c9c50596858eb6d13230f39edbc3173
Ganesh Babu <ganeshbabu346@gmail.com>
2016-07-10 00:07:17 +0530

    ..

--- R/Data Structures/Object_dataframes.R
+++ R/Data Structures/Object_dataframes.R
@@ -4,10 +4,10 @@ studentid <- c(1,2,3,4)
 admindate <- c("10/15/2015","11/16/2015","12/13/2015","14/12/2015")
 studentname <- c("ganesh","star","hari","gana")
 age <- c(12,13,15,17)
-class <- c("Class1","Class2","Class1","Class2")
+classtype <- c("Class1","Class2","Class1","Class2")
 Performance <- c("status","Weak","Good","Excellent")
 #create the data frame object
-studentdata <- data.frame(studentid,admindate,studentname,age,class,Performance) 
+studentdata <- data.frame(studentid,admindate,studentname,age,classtype,Performance) 
 
 
 #Accesing the variable using subscript notations by positional notations of data frame
@@ -23,7 +23,7 @@ studentdata[c("studentid","admindate")]
 studentdata$age
 
 #cross-tabulate the class with performace
-table(studentdata$class,studentdata$Performance)
+table(studentdata$classtype,studentdata$Performance)
 
 
 
