c508bbf54b095e4760218612a65159b37c54e2a2
Ganesh Babu <ganeshbabu346@gmail.com>
2016-07-10 10:40:03 +0530

    ..

--- R/DataInput/Import Data from Database via JDBC.R
+++ R/DataInput/Import Data from Database via JDBC.R
@@ -3,7 +3,7 @@
 #set the path if required and place the Oracle jar
 setwd(file.path("C:","R_Workspace"))
 
-#firstly install the packages for JDBC 
+#firstly install the packages for JDBC  and check the system architecture
 install.packages("RJDBC")
 
 #load the package for these sessions
@@ -19,9 +19,15 @@ con <- dbConnect(drv, "jdbc:oracle:thin:@localhost:1521:orcl", "hr", "hr")# Orac
 con <- dbConnect(drv, "jdbc:mysql://localhost/test", "hr", "hr")# mySQL
 
 #Query the table
-a <- dbGetQuery(con, "select employee_id,first_name,salary from employees")
+a <- dbGetQuery(con, "select * from employees")
 
 #Reading the table directly from the schema
 d <- dbReadTable(con, "employees")
 dbDisconnect(con)
+
+#access data from data frame
+a$FIRST_NAME
+a[c("FIRST_NAME","SALARY")]
+a[2:3]
+a[1,3]
 
--- R/DataInput/Import Data from Database via ODBC.R
+++ R/DataInput/Import Data from Database via ODBC.R
@@ -4,11 +4,24 @@
 install.packages("RODBC")
 
 #Set the ODBC connections to the Using the DSN name
-#Check even the bit machines configurations as well
+#Check even the system architectures configurations as well
 
+#Load the Library
 library(RODBC)
-con <- odbcConnect(dsn="ROracle",uid="hr",pwd ="hr",believeNRows=FALSE)
+
+#URI
+con <- odbcConnect(dsn="ROracle", uid="hr", pwd ="hr", believeNRows=FALSE)
+
+#Query statement
 sqlQuery(con, "select count(*) from employees")
 d <- sqlQuery(con, "select * from employees")
+
+#close the connection
 close(con)
+
+#access data from data frame
+d$FIRST_NAME
+d[c("FIRST_NAME","SALARY")]
+d[2:3]
+d[1,3]
 
