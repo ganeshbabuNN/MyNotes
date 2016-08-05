3c1debdf68cf22586689c55cd8c6624ac7a68c39
Ganesh Babu <ganeshbabu346@gmail.com>
2016-08-05 06:55:55 +0530

    ..

--- R/Control Flow/ControlFlow_Loop_While.R
+++ R/Control Flow/ControlFlow_Loop_While.R
@@ -1,8 +1,13 @@
 # While Statement
 # It executes a statements repetitively until the conditions is no longer true
-i <- 10
-while(i >0)
-{
-  print("Hello")
-  i < i-1
-}
+#statement in samel line sep with ;
+ i <- 10
+ while(i > 0){print("hello");i <- i-1}
+ 
+#without ;
+ i <- 10
+ while(i > 0)
+ {
+   print("hello")
+   i <- i-1
+  }
