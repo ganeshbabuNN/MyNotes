c7c4ec37ddcf970c04206de0d6e1b89466e91c66
ganeshbabug <ganeshbabu346@gmail.com>
2026-01-14 22:22:18 +0530

    .

--- R_Language/2-Programming_Fundamentals/5-DataTypes.R
+++ R_Language/2-Programming_Fundamentals/5-DataTypes.R
@@ -236,7 +236,17 @@ b
 typeof(a)
 class(a) 
 
-a %*% b #dot product
+a %*% b #dot product ex: https://www.mathsisfun.com/algebra/matrix-multiplying.html
+
+#Different behaviour
+a<-matrix(c(1,2,3,4,5,6),nrow = 3,ncol = 3,byrow = TRUE)
+a
+b<- matrix(c(7,8,9,10,11,12),nrow = 3,ncol = 3,byrow = TRUE)
+b
+a*b 
+a+b
+a/b
+a%%b #modulus
 
 #arrays
 #------
